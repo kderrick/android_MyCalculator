89b437b41254826e420be856ef8fe178b1ebaf19
Kyle <kderrick1988@gmail.com>
2016-06-13 21:46:33 -0700

    add addition functionality

--- app/src/main/java/com/epicodus/mycalculator/MainActivity.java
+++ app/src/main/java/com/epicodus/mycalculator/MainActivity.java
@@ -1,13 +1,44 @@
 package com.epicodus.mycalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText operand1;
+    private EditText operand2;
+
+    private Button buttonAddition;
+    private Button buttonSubtraction;
+    private Button buttonMultiplication;
+    private Button buttonDivision;
+
+    private TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        operand1 = (EditText) findViewById(R.id.editOperand1);
+        operand2 = (EditText) findViewById(R.id.editOperand2);
+        buttonAddition = (Button) findViewById(R.id.buttonAddition);
+        buttonSubtraction = (Button) findViewById(R.id.buttonSubtraction);
+        buttonMultiplication = (Button) findViewById(R.id.buttonMultiplication);
+        buttonDivision = (Button) findViewById(R.id.buttonDivision);
+        textResult = (TextView) findViewById(R.id.textResult);
+
+        buttonAddition.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                double oper1 = Double.parseDouble(operand1.getText().toString());
+                double oper2 = Double.parseDouble(operand2.getText().toString());
+                double theResult = oper1 + oper2;
+                textResult.setText(Double.toString(theResult));
+            }
+        });
     }
 }
